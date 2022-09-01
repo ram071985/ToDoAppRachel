84951e9461747641750f0f10667ce53af54f8873
rmuchow95 <rachelmuchow95@gmail.com>
2022-08-31 19:39:50 -0500

    adds ul list and appendChild elements for displaying list.

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-function click()
-{
-  console.log("click");
+const todos = [];
 
+submit.addEventListener("click", function(click) {
+   click.preventDefault();
+    let ulList = document.getElementById("todo-list");
+
+    let li = createListInDOM(click.target.value);
+    ulList.appendChild(li);
+
+    console.log("Click Test: ", ulList);
+  
+});
+
+function createListInDOM(text) {
+  const list = document.createElement("li");
+  const textNode = document.createTextNode(text);
+  list.appendChild(textNode);
+
+  return list;
 }
 
-const button = document.getElementById("button");
-button.addEventListener("click", click);
+function click()
+{ 
+  window.event.preventDefault();
+
+  todos.push("Hello there");
+
+  
+  console.log("To Do Array: ", todos);
+}
+
+
+
+
+
+
+
+//let x = document.toDoEntry.elements[0].text;
+//console.log(x);
